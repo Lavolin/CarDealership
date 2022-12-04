c6d400b82ade2348c801e48e653c65787f69aed5
Lavolin <lavolin@gmail.com>
2022-12-04 16:34:17 +0200

    adding Admin Truck services

--- CarDealership/Areas/Admin/Controllers/TruckController.cs
+++ CarDealership/Areas/Admin/Controllers/TruckController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarDealership.Areas.Admin.Models;
+using CarDealership.Core.Contracts;
+using CarDealership.Core.Services;
+using CarDealership.Extensions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CarDealership.Areas.Admin.Controllers
 {
     public class TruckController : BaseController
     {
-        public IActionResult Index()
+        private readonly ITruckService truckService;
+
+        private readonly IDealerService dealerService;
+
+        public TruckController(ITruckService _truckService, IDealerService _dealerService)
         {
-            return View();
+            truckService = _truckService;
+            dealerService = _dealerService;
+        }
+
+        public async Task<IActionResult> Mine()
+        {
+            var myTrucks = new MyTrucksViewModel();
+            var adminId = User.Id();
+            myTrucks.SoldTrucks = await truckService.AllTrucksByUserId(adminId);
+            var dealerId = await dealerService.GetDealerId(adminId);
+            myTrucks.AddedTrucks = await truckService.AllTrucksByDealerId(dealerId);
+
+            return View(myTrucks);
         }
     }
 }
--- CarDealership/Areas/Admin/Models/MyTrucksViewModel.cs
+++ CarDealership/Areas/Admin/Models/MyTrucksViewModel.cs
@@ -4,8 +4,8 @@ namespace CarDealership.Areas.Admin.Models
 {
     public class MyTrucksViewModel
     {
-        public IEnumerable<TruckServiceModel> AddedCars { get; set; } = new List<TruckServiceModel>();
+        public IEnumerable<TruckServiceModel> AddedTrucks { get; set; } = new List<TruckServiceModel>();
 
-        public IEnumerable<TruckServiceModel> SoldCars { get; set; } = new List<TruckServiceModel>();
+        public IEnumerable<TruckServiceModel> SoldTrucks { get; set; } = new List<TruckServiceModel>();
     }
 }
