97c377365b51133dca93cc5245f6a7250ddbfe15
Lavolin <lavolin@gmail.com>
2022-12-10 15:16:27 +0200

    starting tests

---
+++ CarDealership.UnitTests/CarServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using CarDealership.Core.Contracts;
+using CarDealership.Infrastructure.Data.Common;
+
+namespace CarDealership.UnitTests
+{
+    public class CarServiceTests
+    {
+        private IRepository repo;
+        private ICarService carService;
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
